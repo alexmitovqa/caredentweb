abfcf6d43ba30bee98df21b6f84122840f4e2f59
Alex <aleksandar.mitov@radostinbogdanovsmartit.onmicrosoft.com>
2023-05-04 18:21:51 +0300

    Added some responsiveness and About Us page

--- wwwroot/js/scripts.js
+++ wwwroot/js/scripts.js
@@ -5,3 +5,12 @@ function scrollToElement(elementId) {
         window.scrollTo({top: y, behavior: 'smooth' });
     }
 }
+
+//function initializeGoogleMap() {
+//    var latlng = new google.maps.LatLng(40.716948, -74.003563);
+//    var options = {
+//        zoom: 14, center: latlng,
+//        mapTypeId: google.maps.MapTypeId.ROADMAP
+//    };
+//    var map = new google.maps.Map(document.getElementById("map"), options);
+//}
